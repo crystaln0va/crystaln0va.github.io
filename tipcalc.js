5358c3e85262c68b0c15c64691ccfa6f65767caa
Crystal N0VΛ <91496659+crystaln0va@users.noreply.github.com>
2022-05-01 21:59:38 -0500

    pushypush

---
+++ tipcalc.js
@@ -0,0 +1,26 @@
+function calcTip(){
+
+    /** Gets subtotal input box */
+    var subtotalElem = document.getElementById('subtotal');
+
+    /** Gets tip input box */
+	var tipElem = document.getElementById('tip');
+
+    /** Gets 'total' paragraph */
+	var totalElem = document.getElementById('total');
+
+	var subtotal = parseFloat(subtotalElem.value);
+	var tip = (subtotal * parseFloat(tipElem.value))/100;
+
+    if(isNaN(tip)||isNaN(subtotal)){
+        alert("Please enter a valid value!");
+        tipElem.value='';
+        subtotalElem.value='';
+	    totalElem.innerHTML = '$';
+        return;
+    }
+   
+    var total = subtotal+tip;
+    totalElem.innerHTML = '$' + total;
+
+}
