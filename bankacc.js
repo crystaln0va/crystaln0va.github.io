34af79735080af31f422a58106d739bdc6d61e88
Crystal N0VΛ <91496659+crystaln0va@users.noreply.github.com>
2022-05-03 21:26:01 -0500

    inmybankaccount

---
+++ bankacc.js
@@ -0,0 +1,58 @@
+window.onload = onLoadEvent;
+
+
+
+
+function onLoadEvent() {
+    class Account {
+        #accountName;
+        #balance;
+        static accountInfoList = [];
+    
+        constructor() {
+            
+        }
+        setAccountName(accN) {
+            this.#accountName = accN;
+        }
+        setBalance(bal) {
+            this.#balance = bal;
+        }
+        getAccountName() {
+            return this.#accountName;
+        }
+        getBalance() {
+            return this.#balance;
+        }
+    }
+
+    var createacc_button = document.getElementById('createacc_button');
+    var take_acc_input = document.getElementById('take_acc_input');
+    var take_dep_input = document.getElementById('take_dep_input');
+    var display_textarea = document.getElementById('display_textarea');
+
+    createacc_button.onclick = function() {
+        /** Make a new instance of Account */
+        var acc = new Account();
+        acc.setAccountName(take_acc_input.value);
+        acc.setBalance(take_dep_input.value);
+        Account.accountInfoList.push(acc);
+        
+
+        /** Display the list in display field */
+        display_textarea.textContent = returnItems();
+        /** "Account Name: " + Account.accountInfoList[0].getAccountName() + " Balance: " + Account.accountInfoList[0].getBalance(); */
+
+        function returnItems() {
+            let i;
+            var allDisplay = "";
+            for (i=0; i < Account.accountInfoList.length; i++) {
+                allDisplay = allDisplay + "Account Name: " + Account.accountInfoList[i].getAccountName() + " Balance: " + Account.accountInfoList[i].getBalance() + "\n";
+            }
+            return allDisplay;
+        }
+        
+    }
+
+
+}
